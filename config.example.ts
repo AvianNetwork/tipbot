93967aa6a47329e74906bae428b424264c11b3d0
Bram <84628826+sys-256@users.noreply.github.com>
2022-02-18 18:13:30 +0100

    init typescript, package.json and config.ts files

---
+++ config.example.ts
@@ -0,0 +1,54 @@
+export default {
+    "bot": {
+        "token": "",
+        "prefix": "!",
+    },
+    "project": {
+        "siteurl": "",
+        "githuburl": "",
+        "twitterurl": "",
+        "redditurl": "",
+        "discordurl": "",
+        "telegramurl": "",
+        "telegramannurl": "",
+        "bitcointalkurl": "",
+        "donationaddress": "",
+        "webwalleturl": ""
+    },
+    "coin": {
+        "coinname": "avian",
+        "coinsymbol": "avn",
+        "paytxfee": 0.005,
+        "rpc": {
+            "host": "localhost",
+            "port": 1234,
+            "user": "username",
+            "pass": "password"
+        }
+    },
+    "moderation": {
+        "pm2Name": "TipBot",
+        "perms": [
+            "[MEMBER]"
+        ],
+        "botDev": "[Developer Team]",
+        "logchannel": "",
+        "botspamchannels": [
+            "ID",
+            "ID"
+        ]
+    },
+    "explorer": {
+        "explorerurl": "",
+        "explorertxurl": "",
+        "exploreraddressurl": ""
+    },
+    "wavn": {
+        "contractaddress": "",
+        "polygonapikey": "",
+        "coinwrapurl": ""
+    },
+    "nomics": {
+        "apikey": ""
+    }
+}

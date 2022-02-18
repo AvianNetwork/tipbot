d514290d4287a99ee6612baa4f9951c3a0153c8b
Bram <84628826+sys-256@users.noreply.github.com>
2022-02-18 21:47:15 +0100

    init helper.ts with a simple getTime function

---
+++ src/helper.ts
@@ -0,0 +1,16 @@
+// Import the config variables
+import { config } from "../config.js";
+
+// Import the required packages
+import Discord from "discord.js";
+import fetch from "node-fetch";
+
+import dayjs from "dayjs";
+import dayjs_utc from "dayjs/plugin/utc.js";
+import dayjs_timezone from "dayjs/plugin/timezone.js";
+dayjs.extend(dayjs_utc);
+dayjs.extend(dayjs_timezone);
+
+export const getTime = () => {
+    return dayjs().tz(config.bot.timezone[0]).format(config.bot.timezone[1])
+}
